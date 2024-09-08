5e8b42319647c057480bec5640427a6abbe35b10
Coppermine-SP <son5121502@gmail.com>
2024-09-08 22:06:22 +0900

    feat: 명령 인수로 파일 이름 받기

--- src/viva.c
+++ src/viva.c
@@ -7,20 +7,27 @@
 #include "termui/termui.h"
 #include <stdio.h>
 
+const char* file_name;
+
 bool input_event(char c){
     if(c == CTRL_KEY('q'))return false;
     else if(c == CTRL_KEY('F')){
-        ui_show_prompt("Find");
+        char* str = ui_show_prompt("Find");
+
+        
     }
     return true;
 }
 
 int main(int argc, char* argv[]){
-    te_init(argc < 1 ? argv[1] : NULL);
-    ui_init((argc == 1));
+    file_name = argc > 1 ? argv[1] : NULL;
+    te_init(file_name);
+    ui_init();
 
     //파일을 불러오지 않았을 경우, MOTD 출력
     if(argc == 1) ui_render_motd(true);
+
+    ui_set_status(0, 0, file_name);
 
     ui_input_loop(&input_event);
 }
